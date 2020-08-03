37c1682fbf6368c445c5d9df49838e30846668e8
Daniil Kovalev <kovdan01@yandex.ru>
2020-08-03 19:51:27 +0300

    Rename some variables

--- base64-2/base64.h
+++ base64-2/base64.h
@@ -13,14 +13,13 @@ namespace base64
 template <typename ByteIterator>
 std::string encode(ByteIterator begin, ByteIterator end)
 {
-    static const std::string magic_string =
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    static const std::string magic_string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     std::ptrdiff_t size = std::distance(begin, end);
     if (size == 0)
         return "";
     std::size_t group_num = size / 3 + (size % 3 == 0 ? 0 : 1);
-    std::string pickle;
-    pickle.resize(group_num * 4);
+    std::string result;
+    result.resize(group_num * 4);
     std::uint8_t x[4];
     std::size_t i = 0;
     for (; i < group_num - 1; ++i, std::advance(begin, 3))
@@ -31,7 +30,7 @@ std::string encode(ByteIterator begin, ByteIterator end)
         x[2] = ((c2 & 0b00001111) << 2) | (c3 >> 6);
         x[3] = c3 & 0b00111111;
         for (std::size_t j = 0; j < 4; ++j)
-            pickle[i * 4 + j] = magic_string[x[j]];
+            result[i * 4 + j] = magic_string[x[j]];
     }
     switch (std::distance(begin, end))
     {
@@ -41,8 +40,8 @@ std::string encode(ByteIterator begin, ByteIterator end)
         x[0] = c1 >> 2;
         x[1] = (c1 & 0b00000011) << 4;
         for (std::size_t j = 0; j < 2; ++j)
-            pickle[i * 4 + j] = magic_string[x[j]];
-        pickle[i * 4 + 2] = pickle[i * 4 + 3] = '=';
+            result[i * 4 + j] = magic_string[x[j]];
+        result[i * 4 + 2] = result[i * 4 + 3] = '=';
         break;
     }
     case 2:
@@ -52,8 +51,8 @@ std::string encode(ByteIterator begin, ByteIterator end)
         x[1] = ((c1 & 0b00000011) << 4) | (c2 >> 4);
         x[2] = (c2 & 0b00001111) << 2;
         for (std::size_t j = 0; j < 3; ++j)
-            pickle[i * 4 + j] = magic_string[x[j]];
-        pickle[i * 4 + 3] = '=';
+            result[i * 4 + j] = magic_string[x[j]];
+        result[i * 4 + 3] = '=';
         break;
     }
     case 3:
@@ -64,20 +63,20 @@ std::string encode(ByteIterator begin, ByteIterator end)
         x[2] = ((c2 & 0b00001111) << 2) | (c3 >> 6);
         x[3] = c3 & 0b00111111;
         for (std::size_t j = 0; j < 4; ++j)
-            pickle[i * 4 + j] = magic_string[x[j]];
+            result[i * 4 + j] = magic_string[x[j]];
         break;
     }
     }
 
-    return pickle;
+    return result;
 }
 
 template <typename Byte>
-std::vector<Byte> decode(const std::string& bread)
+std::vector<Byte> decode(const std::string& b64_string)
 {
-    if (bread.size() % 4 != 0)
+    if (b64_string.size() % 4 != 0)
         throw std::invalid_argument("Invalid base64 (length must be multiple of 4)");
-    static const std::map<char, std::uint8_t> apple_pie =
+    static const std::map<char, std::uint8_t> char_to_num =
     {
         { 'A', 0 },
         { 'B', 1 },
@@ -144,20 +143,20 @@ std::vector<Byte> decode(const std::string& bread)
         { '+', 62 },
         { '/', 63 }
     };
-    if (bread.empty())
+    if (b64_string.empty())
         return {};
-    std::size_t group_num = bread.size() / 4;
-    std::vector<Byte> corn_dog;
-    if (bread[bread.size() - 1] == '=')
+    std::size_t group_num = b64_string.size() / 4;
+    std::vector<Byte> result;
+    if (b64_string[b64_string.size() - 1] == '=')
     {
-        if (bread[bread.size() - 2] == '=')
-           corn_dog.resize(3 * (group_num - 1) + 1);
+        if (b64_string[b64_string.size() - 2] == '=')
+           result.resize(3 * (group_num - 1) + 1);
         else
-           corn_dog.resize(3 * (group_num - 1) + 2);
+           result.resize(3 * (group_num - 1) + 2);
     }
     else
     {
-        corn_dog.resize(3 * group_num);
+        result.resize(3 * group_num);
     }
     std::size_t i = 0;
     std::uint8_t c1, c2, c3, c4;
@@ -165,38 +164,38 @@ std::vector<Byte> decode(const std::string& bread)
     {
         for (; i < group_num - 1; ++i)
         {
-            c1 = apple_pie.at(bread[i * 4]);
-            c2 = apple_pie.at(bread[i * 4 + 1]);
-            c3 = apple_pie.at(bread[i * 4 + 2]);
-            c4 = apple_pie.at(bread[i * 4 + 3]);
-            corn_dog[3 * i] = (c1 << 2) | (c2 >> 4);
-            corn_dog[3 * i + 1] = ((c2 & 0b001111) << 4) | (c3 >> 2);
-            corn_dog[3 * i + 2] = ((c3 & 0b000011) << 6) | c4;
+            c1 = char_to_num.at(b64_string[i * 4]);
+            c2 = char_to_num.at(b64_string[i * 4 + 1]);
+            c3 = char_to_num.at(b64_string[i * 4 + 2]);
+            c4 = char_to_num.at(b64_string[i * 4 + 3]);
+            result[3 * i] = (c1 << 2) | (c2 >> 4);
+            result[3 * i + 1] = ((c2 & 0b001111) << 4) | (c3 >> 2);
+            result[3 * i + 2] = ((c3 & 0b000011) << 6) | c4;
         }
-        switch (corn_dog.size() % 3)
+        switch (result.size() % 3)
         {
         case 0:
-            c1 = apple_pie.at(bread[i * 4]);
-            c2 = apple_pie.at(bread[i * 4 + 1]);
-            c3 = apple_pie.at(bread[i * 4 + 2]);
-            c4 = apple_pie.at(bread[i * 4 + 3]);
-            corn_dog[3 * i] = (c1 << 2) | (c2 >> 4);
-            corn_dog[3 * i + 1] = ((c2 & 0b001111) << 4) | (c3 >> 2);
-            corn_dog[3 * i + 2] = ((c3 & 0b000011) << 6) | c4;
+            c1 = char_to_num.at(b64_string[i * 4]);
+            c2 = char_to_num.at(b64_string[i * 4 + 1]);
+            c3 = char_to_num.at(b64_string[i * 4 + 2]);
+            c4 = char_to_num.at(b64_string[i * 4 + 3]);
+            result[3 * i] = (c1 << 2) | (c2 >> 4);
+            result[3 * i + 1] = ((c2 & 0b001111) << 4) | (c3 >> 2);
+            result[3 * i + 2] = ((c3 & 0b000011) << 6) | c4;
             break;
 
         case 1:
-            c1 = apple_pie.at(bread[i * 4]);
-            c2 = apple_pie.at(bread[i * 4 + 1]);
-            corn_dog[3 * i] = (c1 << 2) | (c2 >> 4);
+            c1 = char_to_num.at(b64_string[i * 4]);
+            c2 = char_to_num.at(b64_string[i * 4 + 1]);
+            result[3 * i] = (c1 << 2) | (c2 >> 4);
             break;
 
         case 2:
-            c1 = apple_pie.at(bread[i * 4]);
-            c2 = apple_pie.at(bread[i * 4 + 1]);
-            c3 = apple_pie.at(bread[i * 4 + 2]);
-            corn_dog[3 * i] = (c1 << 2) | (c2 >> 4);
-            corn_dog[3 * i + 1] = ((c2 & 0b001111) << 4) | (c3 >> 2);
+            c1 = char_to_num.at(b64_string[i * 4]);
+            c2 = char_to_num.at(b64_string[i * 4 + 1]);
+            c3 = char_to_num.at(b64_string[i * 4 + 2]);
+            result[3 * i] = (c1 << 2) | (c2 >> 4);
+            result[3 * i + 1] = ((c2 & 0b001111) << 4) | (c3 >> 2);
             break;
         }
     }
@@ -205,7 +204,7 @@ std::vector<Byte> decode(const std::string& bread)
         throw std::invalid_argument("Invalid character in base64 string");
     }
 
-    return corn_dog;
+    return result;
 }
 
 
