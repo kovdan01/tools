f562e720e78c85b037dfd1b48461ab31623c205a
Daniil Kovalev <kovdan01@yandex.ru>
2020-12-04 15:44:11 +0300

    Fix codestyle in split

--- split/main.cpp
+++ split/main.cpp
@@ -1,41 +1,39 @@
-#include "split.h"
+#include "split.hpp"
+
 #include <cassert>
-#include <iostream>
-#include <iterator>
 #include <limits>
-#include <memory>
 #include <random>
 #include <string>
-#include <tuple>
 #include <type_traits>
 #include <utility>
 
-using namespace std;
+static std::mt19937 g_prng(std::random_device{}());  // NOLINT (cert-err58-cpp)
 
 template <typename StringType>
 void test_algo_string(int max_n_splits, int max_part_len)
 {
     using CharType = typename std::remove_reference<decltype (StringType{' '}[0])>::type;
-    std::mt19937 prng (std::random_device{}());
+
+    // Generating data
     std::uniform_int_distribution<int> int_dist(0, max_part_len);
     std::uniform_int_distribution<CharType> char_dist(std::numeric_limits<CharType>::min(), std::numeric_limits<CharType>::max());
-    int n_splits = std::uniform_int_distribution<int>(1, max_n_splits)(prng);
-    CharType sep = char_dist(prng), cur_char;
+    int n_splits = std::uniform_int_distribution<int>(1, max_n_splits)(g_prng);
+    CharType sep = char_dist(g_prng), cur_char;
     int cur_len;
     StringType cur, whole;
     std::vector<StringType> v_true;
     v_true.reserve(static_cast<std::size_t>(n_splits));
     for (int i = 0; i < n_splits; ++i)
     {
-        cur_len = int_dist(prng);
+        cur_len = int_dist(g_prng);
         cur = StringType{};
         for (int j = 0; j < cur_len; ++j)
         {
             do
             {
-                cur_char = char_dist(prng);
+                cur_char = char_dist(g_prng);
             }
-            while(cur_char == sep);
+            while (cur_char == sep);
             cur += cur_char;
         }
         whole += cur;
@@ -43,60 +41,65 @@ void test_algo_string(int max_n_splits, int max_part_len)
             whole += sep;
         v_true.emplace_back(cur);
     }
+
+    // Asserting
     {
-        std::vector<std::basic_string_view<CharType> > v_algo = split::split(whole, sep);
+        std::vector<std::basic_string_view<CharType>> v_algo = my::split(whole, sep);
         assert(v_true.size() == v_algo.size());
         for (std::size_t i = 0; i < v_true.size(); ++i)
             assert(v_algo[i] == std::basic_string_view<CharType>(v_true[i].data(), v_true[i].size()));
     }
     {
         std::size_t cur_i = 0;
-        split::splitter<CharType> algo(whole, sep);
+        my::Splitter<CharType> algo(whole, sep);
         for (auto current : algo)
         {
+            if (cur_i >= v_true.size())
+                assert(false);
             assert(current == std::basic_string_view<CharType>(v_true[cur_i].data(), v_true[cur_i].size()));
             ++cur_i;
         }
         assert(cur_i == v_true.size());
-        cur_i = 0;
-        for (auto it = algo.begin(); it != algo.end(); ++it, ++it)
-        {
-            assert((*it) == std::basic_string_view<CharType>(v_true[cur_i].data(), v_true[cur_i].size()));
-            assert((*it) == std::basic_string_view<CharType>(v_true[cur_i].data(), v_true[cur_i].size()));
-            cur_i += 2;
-        }
     }
 }
 
 template <typename CharType>
 void test_algo_char(int max_n_splits, int max_part_len, bool with_n)
 {
-    //Generating data
-    std::mt19937 prng (std::random_device{}());
+    // Generating data
     std::uniform_int_distribution<int> int_dist(0, max_part_len);
     std::uniform_int_distribution<CharType> char_dist(std::numeric_limits<CharType>::min(), std::numeric_limits<CharType>::max());
-    std::size_t n_splits = static_cast<std::size_t>(std::uniform_int_distribution<int>(1, max_n_splits)(prng));
+    std::size_t n_splits = static_cast<std::size_t>(std::uniform_int_distribution<int>(1, max_n_splits)(g_prng));
     CharType sep, cur_char;
     if (!with_n)
     {
-        do sep = char_dist(prng);
-        while(sep == CharType{0});
+        do
+        {
+            sep = char_dist(g_prng);
+        }
+        while (sep == CharType{0});
     }
     else
-        sep = char_dist(prng);
+    {
+        sep = char_dist(g_prng);
+    }
     std::size_t cur_len, whole_len = 0;
-    std::unique_ptr<CharType[]> whole, cur;
-    std::vector<std::unique_ptr<CharType[]> > v_true(n_splits);
+    std::basic_string<CharType> whole;
+    std::vector<std::basic_string<CharType>> v_true(n_splits);
     std::vector<std::size_t> v_lens(n_splits);
     for (std::size_t i = 0; i < n_splits; ++i)
     {
-        cur_len = static_cast<std::size_t>(int_dist(prng));
+        cur_len = static_cast<std::size_t>(int_dist(g_prng));
         whole_len += cur_len;
-        cur = std::make_unique<CharType[]>(!with_n ? (cur_len + 1) : cur_len);
+        std::basic_string<CharType> cur;
+        cur.resize(!with_n ? (cur_len + 1) : cur_len);
         for (std::size_t j = 0; j < cur_len; ++j)
         {
-            do cur_char = char_dist(prng);
-            while(cur_char == sep || (with_n ? false : (cur_char == CharType{0})));
+            do
+            {
+                cur_char = char_dist(g_prng);
+            }
+            while (cur_char == sep || (with_n ? false : (cur_char == CharType{0})));
             cur[j] = cur_char;
         }
         if (!with_n)
@@ -106,7 +109,7 @@ void test_algo_char(int max_n_splits, int max_part_len, bool with_n)
         v_true[i] = std::move(cur);
         v_lens[i] = cur_len;
     }
-    whole = std::make_unique<CharType[]>(!with_n ? (whole_len + 1) : whole_len);
+    whole.resize(!with_n ? (whole_len + 1) : whole_len);
     std::size_t cur_pos = 0;
     for (std::size_t i = 0; i < v_true.size(); ++i)
     {
@@ -125,57 +128,51 @@ void test_algo_char(int max_n_splits, int max_part_len, bool with_n)
     }
     if (!with_n)
         whole[whole_len] = CharType{0};
-    //Asserting
+
+    // Asserting
     {
-        std::vector<std::basic_string_view<CharType> > v_algo = (with_n ? split::split<CharType, std::size_t>(whole.get(), whole_len, sep) : split::split<CharType>(whole.get(), sep));
+        std::vector<std::basic_string_view<CharType>> v_algo = (with_n ? my::split<CharType, std::size_t>(whole.data(), whole_len, sep) : my::split<CharType>(whole.data(), sep));
         assert(v_true.size() == v_algo.size());
         for (std::size_t i = 0; i < v_true.size(); ++i)
-            assert(v_algo[i] == (with_n ? std::basic_string_view<CharType>(v_true[i].get(), v_lens[i]) : std::basic_string_view<CharType>(v_true[i].get())));
+            assert(v_algo[i] == (with_n ? std::basic_string_view<CharType>(v_true[i].data(), v_lens[i]) : std::basic_string_view<CharType>(v_true[i].data())));
     }
+
     {
         std::size_t cur_i = 0;
         if (!with_n)
         {
-            split::splitter<CharType> algo(whole.get(), sep);
+            my::Splitter<CharType> algo(whole.data(), sep);
             for (auto current : algo)
             {
-                assert(current == std::basic_string_view<CharType>(v_true[cur_i].get()));
+                if (cur_i >= v_true.size())
+                    assert(false);
+                assert(current == std::basic_string_view<CharType>(v_true[cur_i].data()));
                 ++cur_i;
             }
             assert(cur_i == v_true.size());
-            cur_i = 0;
-            for (auto it = algo.begin(); it != algo.end(); ++it, ++it)
-            {
-                assert((*it) == std::basic_string_view<CharType>(v_true[cur_i].get()));
-                assert((*it) == std::basic_string_view<CharType>(v_true[cur_i].get()));
-                cur_i += 2;
-            }
         }
         else
         {
-            split::splitter<CharType> algo(whole.get(), whole_len, sep);
+            my::Splitter<CharType> algo(whole.data(), whole_len, sep);
             for (auto current : algo)
             {
-                assert(current == std::basic_string_view<CharType>(v_true[cur_i].get(), v_lens[cur_i]));
+                if (cur_i >= v_true.size())
+                    assert(false);
+                assert(current == std::basic_string_view<CharType>(v_true[cur_i].data(), v_lens[cur_i]));
                 ++cur_i;
             }
             assert(cur_i == v_true.size());
-            cur_i = 0;
-            for (auto it = algo.begin(); it != algo.end(); ++it, ++it)
-            {
-                assert((*it) == std::basic_string_view<CharType>(v_true[cur_i].get(), v_lens[cur_i]));
-                assert((*it) == std::basic_string_view<CharType>(v_true[cur_i].get(), v_lens[cur_i]));
-                cur_i += 2;
-            }
         }
     }
 }
 
 int main()
 {
-    for (int i = 1; i < 100; ++i)
+    const int max_n_splits_limit = 100;
+    const int max_part_len_limit = 100;
+    for (int i = 1; i < max_n_splits_limit; ++i)
     {
-        for (int j = 0; j < 100; ++j)
+        for (int j = 0; j < max_part_len_limit; ++j)
         {
             test_algo_string<std::string>(i, j);
             test_algo_char<char>(i, j, true);
---
+++ split/split.hpp
@@ -0,0 +1,180 @@
+#ifndef SPLIT_H
+#define SPLIT_H
+
+#include <iterator>
+#include <memory>
+#include <string_view>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+namespace my
+{
+
+namespace detail
+{
+
+template <typename CharType>
+std::vector<std::basic_string_view<CharType>> helper(std::basic_string_view<CharType> str, CharType sep)
+{
+    std::ptrdiff_t cur_sep_ind = -1;
+    std::vector<std::basic_string_view<CharType>> ans;
+    for (std::size_t i = 0; i < str.size(); ++i)
+    {
+        if (str[i] == sep)
+        {
+            ans.emplace_back(str.begin() + cur_sep_ind + 1, i - cur_sep_ind - 1);
+            cur_sep_ind = static_cast<std::ptrdiff_t>(i);
+        }
+    }
+    ans.emplace_back(str.begin() + cur_sep_ind + 1, str.size() - cur_sep_ind - 1);
+    return ans;
+}
+
+template <typename CharType>
+std::pair<std::basic_string_view<CharType>, std::size_t> helper_one_by_one(std::basic_string_view<CharType> str, CharType sep, std::size_t begin = 0)
+{
+    for (std::size_t i = begin; i < str.size(); ++i)
+        if (str[i] == sep)
+            return { std::basic_string_view<CharType>(str.begin() + begin, i - begin), i };
+    return { std::basic_string_view<CharType>(str.begin() + begin, str.size() - begin), str.size() };
+}
+
+}  // namespace detail
+
+template <typename CharType>
+std::vector<std::basic_string_view<CharType>> split(const CharType* str, CharType sep)
+{
+    return detail::helper({ str }, sep);
+}
+
+template <typename CharType, typename SizeType>
+std::vector<std::basic_string_view<CharType>> split(const CharType* str, SizeType size, CharType sep)
+{
+    return detail::helper({ str, size }, sep);
+}
+
+template <typename StringType, typename CharType>
+std::vector<std::basic_string_view<CharType>> split(const StringType& str, CharType sep)
+{
+    return detail::helper({ str.data(), str.size() }, sep);
+}
+
+template <typename CharType, typename StringType = std::basic_string<CharType> >
+class Splitter
+{
+private:
+    std::basic_string_view<CharType> m_str;
+    CharType m_sep;
+
+public:
+    Splitter() = delete;
+
+    Splitter(const CharType* str, CharType sep)
+        : m_str({ str })
+        , m_sep(sep)
+    {
+    }
+
+    Splitter(const CharType* str, std::size_t size, CharType sep)
+        : m_str({ str, size })
+        , m_sep(sep)
+    {
+    }
+
+    Splitter(const StringType& str, CharType sep)
+        : m_str({ str.data(), str.size() })
+        , m_sep(sep)
+    {
+    }
+
+    class Iterator : std::iterator<std::forward_iterator_tag, std::basic_string_view<CharType>, ptrdiff_t>
+    {
+    private:
+        std::size_t m_begin = 0, m_end = -1;
+        Splitter<CharType, StringType>& m_obj;
+        bool m_is_calculated = false;
+
+        Iterator(std::size_t begin, std::size_t end, Splitter<CharType, StringType>& obj) noexcept
+            : m_begin(begin)
+            , m_end(end)
+            , m_obj(obj)
+        {
+        }
+
+        std::basic_string_view<CharType> get_next()
+        {
+            for (std::size_t i = m_begin; i < m_obj.m_str.size(); ++i)
+            {
+                if (m_obj.m_str[i] == m_obj.m_sep)
+                {
+                    m_end = i;
+                    return { m_obj.m_str.begin() + m_begin, i - m_begin };
+                }
+            }
+            m_end = m_obj.m_str.size();
+            return { m_obj.m_str.begin() + m_begin, m_obj.m_str.size() - m_begin };
+        }
+
+    public:
+        friend class Splitter<CharType, StringType>;
+
+        Iterator() noexcept = delete;
+        Iterator(const Iterator&) noexcept = default;
+        Iterator& operator=(const Iterator&) noexcept = default;
+        Iterator(Iterator&&) noexcept = default;
+        Iterator& operator=(Iterator&&) noexcept = default;
+        ~Iterator() = default;
+
+        std::basic_string_view<CharType> operator*()
+        {
+            if (m_is_calculated)
+                return { m_obj.m_str.begin() + m_begin, m_end - m_begin };
+            m_is_calculated = true;
+            return get_next();
+        }
+
+        Iterator& operator++()
+        {
+            if (!m_is_calculated)
+                get_next();
+            m_begin = (m_end == m_obj.m_str.size() ? m_obj.m_str.size() : m_end + 1);
+            m_is_calculated = false;
+            return *this;
+        }
+
+        Iterator operator++(int) { auto old = *this; ++(*this); return old; }  // NOLINT (cert-dcl21-cpp)
+
+        friend void swap(Iterator& lhs, Iterator& rhs)
+        {
+            std::swap(lhs.m_begin, rhs.m_begin);
+            std::swap(lhs.m_end, rhs.m_end);
+            std::swap(lhs.m_obj, rhs.m_obj);
+            std::swap(lhs.m_is_calculated, rhs.m_is_calculated);
+        }
+
+        friend bool operator==(const Iterator& lhs, const Iterator& rhs)
+        {
+            return std::tie(lhs.m_begin, lhs.m_end) == std::tie(rhs.m_begin, rhs.m_end);
+        }
+
+        friend bool operator!=(const Iterator& lhs, const Iterator& rhs)
+        {
+            return !(lhs == rhs);
+        }
+    };
+
+    Iterator begin()
+    {
+        return Iterator(0, -1, *this);
+    }
+
+    Iterator end()
+    {
+        return Iterator(m_str.size(), m_str.size(), *this);
+    }
+};
+
+}  // namespace my
+
+#endif  // SPLIT_H
